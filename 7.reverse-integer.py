0848861cd24becb9ce1b0813f110d553a5e7a86e
shhqq <shhicc@163.com>
2019-11-08 11:15:38 +0800

    Problem 7, 9

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -7,6 +7,33 @@
 # @lc code=start
 class Solution:
     def reverse(self, x: int) -> int:
-        while
+        x_str = str(x)
+        length = len(x_str)
+        result = ""
+        for i in range(length):
+            result += x_str[length - 1 - i]
+        if x < 0:
+            result = "-" + result[0:length-1]
+        result = int(result)
+        if result < -pow(2, 31) or result > pow(2, 31) - 1:
+            return 0
+        else:
+            return result
+        
+        # result = 0
+        # temp = abs(x)
+        # while True:
+        #     result = result * 10 + temp % 10
+        #     if temp < 10:
+        #         break
+        #     else:
+        #         temp = temp // 10
+        # if x < 0:
+        #     result = -result
+        # if result < -pow(2, 31) or result > pow(2, 31) - 1:
+        #     return 0
+        # else:
+        #     return result
+
 # @lc code=end
 
---
+++ 9.palindrome-number.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode id=9 lang=python3
+#
+# [9] Palindrome Number
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        length = 1
+        while x // length >= 10:
+            length *= 10
+        
+        while x:
+            if x % 10 != x // length:
+                return False
+            x = (x % length) // 10
+            length /=100
+
+        return True
+
+
+        # if x < 0:
+        #     return False
+        # length = 0
+        # while True:
+        #     if x // pow(10, length) > 0:
+        #         length += 1
+        #     else:
+        #         break
+        # for i in range(length // 2):
+        #     if x % 10 != x // pow(10, length - i * 2 - 1):
+        #         return False
+        #     else:
+        #         x = (x % pow(10, length - 1 - i * 2)) // 10
+        # return True
+
+        # xlist = []
+        # xtemp = x
+        # if xtemp<0:
+        #     return False
+        # while True:
+        #     xlist.append(xtemp % 10)
+        #     if xtemp < 10:
+        #         break
+        #     else:
+        #         xtemp = xtemp // 10
+        # for i in range(len(xlist) // 2):
+        #     if xlist[i] != xlist[len(xlist) - 1 - i]:
+        #         return False
+        # return True
+
+
+# @lc code=end
+
