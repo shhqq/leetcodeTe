af7d775e72fbb300a638a963060b1b689908286a
shhqq <shhicc@163.com>
2020-01-02 22:18:40 +0800

    Problem 367

--- 367.valid-perfect-square.py
+++ 367.valid-perfect-square.py
@@ -7,6 +7,21 @@
 # @lc code=start
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
+# Use dichotomy.
+        if num == 0 or num == 1:
+            return True
+        left = 0
+        right = num
+        mid = (left + right) // 2
+        while left < mid:
+            if num < pow(mid, 2):
+                right = mid
+            elif num > pow(mid, 2):
+                left = mid
+            else:
+                return True
+            mid = (left + right) // 2
+        return False
         
 # @lc code=end
 
